bc0a37901950190a8c8c928dcbf825d7686b281f
gmuerov <gmuerov@gmail.com>
2015-09-16 18:54:07 +0300

    Added calculations for 2 and 3D

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include "KineticAlphaCompex.h"
 #include <CGAL/Kinetic/Exact_simulation_traits.h>
+#include <CGAL/Kinetic/Cartesian.h>
+#include "ShortCalculationStructs.h"
 
 int main()
 {
-	CGAL::Kinetic::Exact_simulation_traits tr(0, 1000);
-
-
+	typedef CGAL::Kinetic::Exact_simulation_traits Traits;
+	Traits tr(0, 1000);
 
 	KineticAlphaComplex a(tr, 2.0);
 	std::cout<<"It runs!";
